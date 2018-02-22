3d7e1908ae5926505f462880949e2e715aa0e366
Bartłomiej Bajda <allerbbb@gmail.com>
2018-02-22 00:53:19 +0000

    first update

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function getJoke() {
+  var url = 'http://api.icndb.com/jokes/random';
+  var paragraph = document.getElementById('joke');
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url);
+  xhr.addEventListener('load', function() {
+    var response = JSON.parse(xhr.response);
+    paragraph.innerHTML = response.value.joke;
+  });
+  xhr.send();
+}
+window.onload(getJoke());
+//var button = document.getElementById('get-joke');
+//button.addEventListener('click', getJoke);
